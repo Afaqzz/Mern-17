f1d70c249a319e67c23a611bded2086bfe049868
afaqzz <afaq.ahmad411@icloud.com>
2024-05-02 11:56:36 +0500

    merge conflict ressolved

--- factorial.js
+++ factorial.js
@@ -1,6 +1,9 @@
 const primeNumber = () => {
   console.log("primeNumber");
 };
+const pallindrome = () => {
+  console.log("pallindrome");
+};
 
 const factorial = () => {
   console.log("factorial");
